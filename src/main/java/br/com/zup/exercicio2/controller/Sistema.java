8c68d5d7d023f9a868fbb7c4d6ac2803fab646f2
Larissa Novais <larinovais54@gmail.com>
2021-10-29 13:43:51 -0300

    validar o email

--- src/main/java/br/com/zup/exercicio2/controller/Sistema.java
+++ src/main/java/br/com/zup/exercicio2/controller/Sistema.java
@@ -1,8 +1,9 @@
 package br.com.zup.exercicio2.controller;
 
 import br.com.zup.exercicio2.controller.dtos.LeadDTO;
-import br.com.zup.exercicio2.controller.dtos.ProdutoDTO;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,11 +15,16 @@ public class Sistema {
 
 
     public List<LeadDTO> mostrarLead() {
-        return  leadsDTOS;
+        return leadsDTOS;
     }
+
     public void cadastrarLead(LeadDTO leadDTO) {
+        for (LeadDTO referencia : leadsDTOS) {
+            if (referencia.getEmailDoLead().equalsIgnoreCase(leadDTO.getEmailDoLead())) {
+               throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+            }
+        }
         leadsDTOS.add(leadDTO);
     }
-
-
 }
+
